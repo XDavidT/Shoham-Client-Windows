42256df87f329d8bf4b938b9722b9f93c0a3a071
Saharh <sahari6767@gmail.com>
2019-06-06 19:01:05 +0300

    Test

--- EvtReader.py
+++ EvtReader.py
@@ -33,3 +33,5 @@ def GetEvents():
         #Now need to move this slave using protoBuf #ToDo
         last_size += 1
         print("---------------------------------------------------------------------------------\n")
+
+        #Testing
