1ae9213fc8c1649dd1765435ce3cc6f3e9bcbed1
Saharh <sahari6767@gmail.com>
2019-06-14 15:00:47 +0300

    Fixed the events buffer

--- EvtReader.py
+++ EvtReader.py
@@ -1,35 +1,35 @@
 import win32evtlog # requires pywin32 pre-installed
 import time        # Must have to wait
-from EvtClass import Evt
+
 
 def GetEvents():
     server = "localhost"  # name of the target computer to get event logs
     logtype = "Security"  # 'Application' # 'Security' # 'System'
 
     hand = win32evtlog.OpenEventLog(server, logtype)  # Handle the connection
-    flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
-    curr_size = win32evtlog.GetNumberOfEventLogRecords(hand)
-    last_size = curr_size - 1
-    # file1 = open("textfile.txt", 'w+')
+    flags = win32evtlog.EVENTLOG_FORWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
+    last_check = win32evtlog.GetNumberOfEventLogRecords(hand)
 
     while True:
-        while curr_size == last_size:  # Get only new logs+
-            time.sleep(1)
-            curr_size = win32evtlog.GetNumberOfEventLogRecords(hand)
-        events = win32evtlog.ReadEventLog(hand, flags, 0)
-        if events:
-            for event in events:
-                # print("Event Category: " ,event.EventCategory)
-                # print("Time Generated: ", event.TimeGenerated)
-                # print("Source Name: " , event.SourceName)
-                # print("Event ID: " , event.EventID)
-                # print("Event Type: " , event.EventType)
-                data = event.StringInserts
-                if data:
-                    data_content = ""
-                    for msg in data:
-                        data_content += msg
-        slave = Evt(event.EventID,event.TimeGenerated,event.EventType,event.EventCategory,event.SourceName,data_content)
-        #Now need to move this slave using protoBuf #ToDo
-        last_size += 1
-        print("---------------------------------------------------------------------------------\n")
+            curr_check = win32evtlog.GetNumberOfEventLogRecords(hand)
+            if curr_check > last_check:
+                events = win32evtlog.ReadEventLog(hand,flags,curr_check)
+                for event in events:
+                    print("Event Category: ", event.EventCategory)
+                    print("Time Generated: ", event.TimeGenerated)
+                    print("Source Name: " , event.SourceName)
+                    print("Event ID: " , event.EventID)
+                    print("Event Type: " , event.EventType)
+                    print("\n")
+                    data = event.StringInserts
+                    if data:
+                        data_content = "Data: "
+                        for msg in data:
+                            data_content += msg+"\n"
+                        print(data_content)
+                    print("---------------------------------------------------------------------------------\n")
+                last_check = curr_check
+        # slave = Evt(event.EventID,event.TimeGenerated,event.EventType,event.EventCategory,event.SourceName,data_content)
+        #Now need to move this slave using protoBuf
+
+
