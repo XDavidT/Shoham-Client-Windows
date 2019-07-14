2a75ed884f878161736e1e60194bcbeedffb0bda
David Tayar <xdavidt@gmail.com>
2019-07-14 13:42:50 +0300

    Added protobuf functions

--- EvtClass.py
+++ EvtClass.py
@@ -1,12 +1,12 @@
-import platform
-
-class Evt:
-    def __init__(self,id,time,type = None,cat = None,src = None, data = None):
-        self.id = id
-        self.time = time
-        self.type = type
-        self.cat = cat
-        self.src = src
-        self.data = data
-        self.os = platform.system()
-
+# import platform
+#
+# class Evt:
+#     def __init__(self,id,time,type = None,cat = None,src = None, data = None):
+#         self.id = id
+#         self.time = time
+#         self.type = type
+#         self.cat = cat
+#         self.src = src
+#         self.data = data
+#         self.os = platform.system()
+#
--- EvtReader.py
+++ EvtReader.py
@@ -1,35 +1,50 @@
 import win32evtlog # requires pywin32 pre-installed
-import time        # Must have to wait
+import evtmanager_pb2
 
 server = "localhost"  # name of the target computer to get event logs
 logtype = "Security"  # 'Application' # 'Security' # 'System'
 
-def GetEvents():
+def GetEvents(evtMgr):
     hand = win32evtlog.OpenEventLog(server, logtype)  # Handle the connection
     flags = win32evtlog.EVENTLOG_FORWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
     last_check = win32evtlog.GetNumberOfEventLogRecords(hand)
-
     while True:
             curr_check = win32evtlog.GetNumberOfEventLogRecords(hand)
             if curr_check > last_check:
-                events = win32evtlog.ReadEventLog(hand,flags,curr_check)
+                events = win32evtlog.ReadEventLog(hand, flags, curr_check)
                 for event in events:
-                    print("Event Category: ", event.EventCategory)
-                    print("Time Generated: ", event.TimeGenerated)
-                    print("Source Name: " , event.SourceName)
-                    print("Event ID: " , event.EventID)
-                    print("Event Type: " , event.EventType)
-                    print("\n")
+                    # print(type(event.EventID))
+                    evtMgr.id = event.EventID
+                    evtMgr.time.FromDatetime(event.TimeGenerated)
+                    # print(type(event.EventType))
+                    evtMgr.type = event.EventType
+                    evtMgr.src = event.SourceName
+                    evtMgr.cat = event.EventCategory
                     data = event.StringInserts
                     if data:
-                        data_content = "Data: "
                         for msg in data:
-                            data_content += msg+"\n"
-                        print(data_content)
+                            data_input = evtMgr.dataList.add()
+                            data_input.data = msg
+                    print(evtMgr.SerializeToString())
                     print("---------------------------------------------------------------------------------\n")
                 last_check = curr_check
+
         # slave = Evt(event.EventID,event.TimeGenerated,event.EventType,event.EventCategory,event.SourceName,data_content)
         #Now need to move this slave using protoBuf
+
+
+
+evtMgr = evtmanager_pb2.evtMgr()
+GetEvents(evtMgr)
+
+
+
+
+
+
+
+
+
 
 
 def clearEvt():
